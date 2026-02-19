b0d34f9cb1031f78569fded529f476ccf8967068
muhammadfauzi440 <muhammadfauzi9953@gmail.com>
2026-02-19 11:21:05 +0700

    nambah dokumen di admin controller

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -16,7 +16,7 @@ class AdminController extends Controller
 {
     public function index()
     {
-        $pendaftarans = Pendaftaran::with(['user', 'instansi'])->latest()->get();
+        $pendaftarans = Pendaftaran::with(['user', 'instansi', 'dokumen'])->latest()->get();
 
         return view('admin.pendaftaran.index', compact('pendaftarans'));
     }
