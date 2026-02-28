0f2b68002f3b56e93175ae3fbede20c48a8a8cc8
muhammadfauzi440 <muhammadfauzi9953@gmail.com>
2026-02-28 20:59:30 +0700

    ubah warna button export pdf menjadi merah

--- resources/views/admin/pendaftaran/index.blade.php
+++ resources/views/admin/pendaftaran/index.blade.php
@@ -18,7 +18,7 @@
                     </a>
 
                     <a href="{{ route('admin.export.pdf') }}"
-                        class="flex items-center gap-2 bg-blue-600 text-white px-6 py-3 rounded-2xl text-[10px] font-black uppercase tracking-[0.2em] hover:bg-blue-700 transition-all shadow-lg shadow-blue-600/20">
+                        class="flex items-center gap-2 bg-red-600 text-white px-6 py-3 rounded-2xl text-[10px] font-black uppercase tracking-[0.2em] hover:bg-red-700 transition-all shadow-lg shadow-blue-600/20">
                         <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z" />
